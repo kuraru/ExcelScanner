92e1dbeebc7af04612cd3812b117f1a023917682
Luis Rodriguez <kuraru@gmail.com>
2024-07-02 06:12:13 -0600

    Se agrego el otro main. Se agregaron cambios al main principal.

--- main_otro.py
+++ main_otro.py
@@ -7,23 +7,34 @@ from typing import Optional, TextIO
 import easyocr
 
 MIN_POINTS_PER_LINE = 15
-STOP_WORDS = ["CONCLUIDO", "1"]
-START_FORMATS = ["^[0-9].*/[0-9].*$", "^[0-9].*"]
+STOP_WORDS = ["UIDO", "NADO", "VIDAD"]
+START_FORMATS = ["^[0-9].*/[0-9].*$"]
 
-# Headers formato unico para la remision de documentos base de la accion en materia civil
+#Headers formato unico para la remision de documentos base de la accion en materia civil
 rawHeaders = ['EXPEDIENTE', 'NÚM DE SOBRES', 'ACTOR', 'DEMANDADO', 'MOTIVO DE RESGUARDO']
 newHeaders = ['EXPEDIENTE', 'ACTOR', 'DEMANDADO', 'MOTIVO DE RESGUARDO', 'PADDING', 'NÚM DE SOBRES']
 
 
 def separate_by_line(ocr_output: list) -> list:
-    return [entry[1] for entry in ocr_output]
-
+    last_y = 0
+    lines = []
+    line = []
+    for entry in ocr_output:
+        current_y = entry[0][0][1]
+        if last_y - current_y >= MIN_POINTS_PER_LINE:
+            if len(line):
+                lines.append(line)
+            line = []
+            last_y = current_y
+        line.append(entry[1])
+    if len(line):
+        lines.append(line)
+    return lines
 
 def removeHeaders(headers: list, data: list):
     for header in headers:
         data.remove(header)
     return data
-
 
 def create_string_data(data: list):
     itemLineStr = ''
@@ -31,15 +42,15 @@ def create_string_data(data: list):
     f = open("data.csv", "w")
     for dataItem in data:
         print(dataItem)
-        if (countItemsLine == 0 or countItemsLine == 4):
+        if(countItemsLine == 0 or countItemsLine == 4):
             itemLineStr = '' + dataItem
             countItemsLine += 1
-        elif (countItemsLine > 0 and countItemsLine <= 3):
+        elif(countItemsLine > 0 and countItemsLine <= 3):
             itemLineStr = itemLineStr + "," + dataItem
-            countItemsLine += 1
-            # print('countItemsLine', countItemsLine)
+            countItemsLine += 1 
+            #print('countItemsLine', countItemsLine)
         else:
-            # itemLineStr = itemLineStr + ' /'
+            #itemLineStr = itemLineStr + ' /'
             f.write(f"{itemLineStr}\n")
             countItemsLine = 0
             itemLineStr = ''
@@ -47,11 +58,23 @@ def create_string_data(data: list):
     f.close()
 
 
+def find_endswith(data, ending) -> int:
+    pos = 0
+    for dataItem in data:
+        if dataItem.endswith(ending):
+            return pos
+        pos += 1
+    raise ValueError("Value does not end with ending")
+
+
 def get_nearest_stop(data: list[str]) -> int:
     positions = []
     for word in STOP_WORDS:
         print(word)
-        pos = data.index(word)
+        try:
+            pos = find_endswith(data, word)
+        except ValueError:
+            pos = 10000
         print(pos)
         positions.append(pos)
     return min(positions)
@@ -72,28 +95,27 @@ def get_nearest_start(data: list[str]) -> int:
     raise ValueError("Regex did not find anything.")
 
 
-def filter_chars(line: list[str], chars: dict[str, str]) -> list[str]:
-    new_list = line
-    for char, repl in chars.items():
-        new_list = [element.replace(char, repl) for element in line]
-    return new_list
-
-
 def write_row(file: TextIO, line: list[str]) -> None:
-    line = filter_chars(line, {"@": "O", "{": "", "}": "", '"': ''})
+    for element in line:
+        if "@" in element:
+            element.replace("@", "")
     this_line = ",".join(line)
     this_line += "," * (len(newHeaders) - len(line)) + "1" + "\n"
     file.write(this_line)
 
 
-def create_string_data_2(data: list[str], f: TextIO, page: Optional[int] = 0) -> None:
+def create_string_data_2(data: list[str], f: TextIO, page: Optional[int]=0) -> None:
     local_data = data
     while len(local_data) > 0:
         try:
             pos = get_nearest_stop(local_data)
             print("pos: ", pos)
             if pos > 5:
-                pos = get_nearest_start(local_data)
+                old_pos = pos
+                try:
+                    pos = get_nearest_start(local_data)
+                except ValueError:
+                    pos = 6
                 if pos:
                     items = local_data[:pos]
                     local_data = local_data[pos:]
@@ -109,24 +131,24 @@ def create_string_data_2(data: list[str], f: TextIO, page: Optional[int] = 0) ->
         write_row(f, items)
 
 
-def get_dir_name(full_path: str) -> str:
-    return full_path.split("/")[-1]
-
-
 def run_all_over_dir(dir: str, reader: easyocr.Reader) -> None:
     files = next(os.walk(dir), (None, None, []))[2]
 
-    file_num = 1
-    csv_file_name = dir + "/" + get_dir_name(dir) + ".csv"
-    with open(csv_file_name, "w") as f:
+    file_num = 0
+    filename = dir + "/" + dir.split("/")[-1] + ".csv"
+    with open(filename, "w") as f:
         f.write(",".join(newHeaders) + "\n")
         for file in files:
-            if file.endswith(".jpeg") or file.endswith(".jpg"):
+            if file.endswith(".jpg") or file.endswith(".jpeg"):
                 f_result = reader.readtext(dir + "/" + file)
 
-                data = separate_by_line(f_result)
-                pos = get_nearest_start(data)
-                data = data[pos:]
+                data = separate_by_line(f_result)[0]
+                try:
+                    pos = get_nearest_start(data)
+                except ValueError:
+                    data = []
+                else:
+                    data = data[pos:]
 
                 create_string_data_2(data, f, page=file_num)
                 file_num += 1
@@ -137,13 +159,16 @@ if __name__ == "__main__":
     reader = easyocr.Reader(['es'])
 
     # This is the inout dir
-    input_dir = "Sobres/122_TERCERO FAMILIAR DE ECATEPEC_2016-2020_T1_SOBRES-20240628T013712Z-001/122_TERCERO FAMILIAR DE ECATEPEC_2016-2020_T1_SOBRES/SOBRES_20231005-105426-038"
+    # input_dir = "Sobres/108_SEGUNDO CIVIL DE ECATEPEC_2016-2020_T1_SOBRES/SOBRES_20231009-175812-186"
+    # input_dir = "Sobres/108_SEGUNDO CIVIL DE ECATEPEC_2016-2020_T1_SOBRES/SOBRES_20231009-181355-189"
+    input_dir = "Sobres/108_SEGUNDO CIVIL DE ECATEPEC_2016-2020_T1_SOBRES/SOBRES_20231009-182251-195"
 
     # check input arguments
     if len(sys.argv) > 1:
         input_dir = sys.argv[1]
 
-    local_path = os.path.abspath(__file__).split("\\")[:-1]
+    local_path = os.path.abspath(__file__).replace("\\", "/")
+    local_path = local_path.split("/")[:-1]
     local_path = "/".join(local_path) + "/" + input_dir
 
     run_all_over_dir(local_path, reader)
